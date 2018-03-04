ddea66d0e10c8a7ca35d58a2ab6c32e9e8576451
Thiago Goulart <thiago@penseavanti.com.br>
2018-03-03 21:32:40 -0300

    created-config-module

---
+++ src/Thiagoalgo/Vtex/Config/Config.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Thiagoalgo\Vtex\Config;
+
+use Noodlehaus\Config as ConfigLoader;
+
+class Config {
+
+    public $account;
+    public $environment;
+    public $key;
+    public $token;
+
+    public function __construct($file = null) {
+        $this->loadProperties($file);
+    }
+
+    private function loadProperties($file) {
+        if (!is_null($file)) {
+            $config = ConfigLoader::load($file);
+        } else {
+            $config = ConfigLoader::load('vtex.json');
+        }
+
+        $reflect = new \ReflectionClass($this);
+        $props = $reflect->getProperties(\ReflectionProperty::IS_PUBLIC);
+
+        foreach ($props as $prop) {
+            $property = $prop->getName();
+            if ($config->has($property)) {
+                $this->{$property} = $config->get($property);
+            }
+
+        }
+
+    }
+
+}
