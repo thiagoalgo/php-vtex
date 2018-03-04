2e2a18548b68a843419a072d304eabd9b21c8f8d
Thiago Goulart <thiago@penseavanti.com.br>
2018-03-04 00:23:52 -0300

    created method get on Oms Order class

--- index.php
+++ index.php
@@ -3,9 +3,14 @@ define("DS", DIRECTORY_SEPARATOR);
 require __DIR__ . DS . "vendor" . DS . "autoload.php";
 
 use Thiagoalgo\Vtex\Config\Config;
+use Thiagoalgo\Vtex\Api\Oms\Order;
 
 $config = new Config('vtex.json');
 var_dump($config);
 
 $config = new Config();
 var_dump($config);
+
+$order = new Order($config);
+$ord = $order->get('v1850336lbll-01');
+var_dump($ord);
--- src/Thiagoalgo/Vtex/Api/Oms/Order.php
+++ src/Thiagoalgo/Vtex/Api/Oms/Order.php
@@ -3,10 +3,39 @@
 namespace Thiagoalgo\Vtex\Api\Oms;
 
 
+use GuzzleHttp\Client;
+use Thiagoalgo\Vtex\Config\Config;
+
 class Order {
 
-    function __construct() {
-        echo "Order class";
+    private $config;
+    private $httpClient;
+
+    public function __construct(Config $config) {
+        $this->config = $config;
+        $this->httpClient = new Client();
+    }
+
+    public function get($orderId) {
+        /*curl --request GET \
+        --url 'http://{{accountName}}.{{environment}}.com.br/api/oms/pvt/orders/{{orderId}}' \
+        --header 'Accept: application/json' \
+        --header 'Content-Type: application/json' \
+        --header 'X-VTEX-API-AppKey: {{X-VTEX-API-AppKey}}' \
+        --header 'X-VTEX-API-AppToken: {{X-VTEX-API-AppToken}}'*/
+
+        $url = $this->config->getBaseUrl() . "/api/oms/pvt/orders/" . $orderId;
+        $response = $this->httpClient->request('GET', $url, [
+           'headers' => [
+               'Accept' => 'application/json',
+               'Content-Type' => 'application/json',
+               'X-VTEX-API-AppKey' => $this->config->key,
+               'X-VTEX-API-AppToken' => $this->config->token
+           ]
+        ]);
+
+        return json_decode($response->getBody()->getContents(), true);
+
     }
 
 } 
